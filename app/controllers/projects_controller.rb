c371098c100cf0f6b3f829f03c00988ba2ea25fa
minguu <minguu42@gmail.com>
2020-09-24 12:16:18 +0900

    プロジェクトを作成する機能を実装

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,14 +1,23 @@
 class ProjectsController < ApplicationController
   def index
+    @projects = Project.all
   end
 
   def show
   end
 
   def new
+    @project = Project.new
   end
 
   def create
+    @project = Project.new(project_params)
+    if @project.save
+      flash[:success] = "プロジェクトを作成しました"
+      redirect_to projects_url
+    else
+      render "projects/new"
+    end
   end
 
   def edit
@@ -19,4 +28,9 @@ class ProjectsController < ApplicationController
 
   def destroy
   end
+
+  private
+    def project_params
+      params.require(:project).permit(:name, :description, :color_id)
+    end
 end
