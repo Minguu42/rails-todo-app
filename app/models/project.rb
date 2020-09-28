8fadf814fb7289462d65ad4a274167c73d1d3fc0
minguu <minguu42@gmail.com>
2020-09-29 07:19:18 +0900

    ProjectモデルとTaskモデルに一対多の関係をつける

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,6 @@
 class Project < ApplicationRecord
   belongs_to :user
+  has_many :tasks
 
   validates :name, presence: true, length: {maximum: 50}
   validates :description, length: {maximum: 200}
