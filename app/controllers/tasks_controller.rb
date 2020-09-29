84fe042c0f1a199d84fcd226f64c30ef32a8d93b
minguu <minguu42@gmail.com>
2020-09-29 15:11:10 +0900

    Taskを作成するフォームを追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,7 @@ class TasksController < ApplicationController
     @project = Project.find(params[:task][:project_id])
     @task = @project.tasks.build(task_params)
     if @task.save
-      flash[:success] = 'タスクを削除しました'
+      flash[:success] = 'タスクを作成しました'
       redirect_to project_url(@project.id)
     else
       render 'projects/show'
@@ -14,7 +14,7 @@ class TasksController < ApplicationController
   def destroy
     Task.find(params[:id]).destroy
     flash[:success] = 'タスクを削除しました'
-    redirect_to root_url
+    redirect_back(fallback_location: root_url)
   end
 
   private
