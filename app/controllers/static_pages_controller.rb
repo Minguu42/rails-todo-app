dc8b653b61f4053ac0ab3c8eab5dbdd4310014f6
minguu <minguu42@gmail.com>
2020-09-30 07:37:25 +0900

    kaminariによるページネーションの実装

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,9 +2,9 @@ class StaticPagesController < ApplicationController
   def home
     if user_signed_in?
       @user = current_user
-      @projects = @user.projects
+      @projects = @user.projects.page(params[:page]).per(10)
     else
-      @projects = Project.all
+      @projects = Project.all.page(params[:page]).per(10)
     end
   end
 end
